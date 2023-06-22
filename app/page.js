ea131bc3c580d7616c2983f798ea7a487f3c1c68
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-06-23 00:57:08 +0100

    bootstrapped nextjs project

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Header } from '@/components/Header'
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Header />
+    </main>
+  )
+}

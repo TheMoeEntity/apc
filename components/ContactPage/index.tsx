caf4f5c9aa9ae68436941e0e57bd544a58c9a71a
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-08-23 18:12:13 +0100

    reduced size of maps in contact page

--- components/ContactPage/index.tsx
+++ components/ContactPage/index.tsx
@@ -36,7 +36,7 @@ const Contact = () => {
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.0775319777313!2d-118.40832329999999!3d34.0675267!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bbf626255555%3A0x5baf12b26c82f470!2s9777%20Wilshire%20Blvd%20Suite%20400%2C%20Beverly%20Hills%2C%20CA%2090210%2C%20USA!5e0!3m2!1sen!2sng!4v1692804334644!5m2!1sen!2sng"
               width="100%"
-              height="550"
+              height="480"
               frameBorder="0"
               loading="lazy"
               style={{ border: 0 }}

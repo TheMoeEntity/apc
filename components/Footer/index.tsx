dddf613229c10b3c0ea0b3af6d98abfda30b47ae
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-09-20 17:59:57 +0100

    changed logo for footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,5 @@
 import styles from "../../app/page.module.css";
-import logo from "../../public/images/logo-option.png";
+import logo from "../../public/images/logo-option3.png";
 import Image from "next/image";
 import Link from "next/link";
 
